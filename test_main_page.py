da7e38894ea01a0862a950602fca5c770f121342
Irina Popova <irina.popova.gdc@fujitsu.com>
2022-08-11 15:24:59 +0300

    Base page added

--- test_main_page.py
+++ test_main_page.py
@@ -5,9 +5,11 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login_page(browser)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
-
 
 
 
