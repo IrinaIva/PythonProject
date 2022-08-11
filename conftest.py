9f24cf5c030c480d0de7c62e20df67d036a0fe9f
Irina Popova <irina.popova.gdc@fujitsu.com>
2022-08-11 15:11:43 +0300

    task 4.1 Что такое Page Object?

--- conftest.py
+++ conftest.py
@@ -3,14 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose user_language: en, ru ...")
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    browser = None
+
+    browser_name = request.config.getoption("browser_name")
+    if browser_name is None:
+        browser_name = "chrome"
+
+    if user_language is None:
+        user_language = "en"
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = webdriver.ChromeOptions()
@@ -28,5 +36,6 @@ def browser(request):
     print("\nquit browser..")
     browser.quit()
 
-    #pytest -s -v --browser_name=chrome test_parser.py
+    # pytest -s -v --browser_name=chrome test_parser.py
+    # pytest --language=es test_items.py
 
