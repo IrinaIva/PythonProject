8c00de959df3580584f46ae02f100de0e812936b
Irina Popova <irina.popova.gdc@fujitsu.com>
2022-09-22 23:18:33 +0300

    Финишная прямая: готовим код к ревью

--- test_product_page.py
+++ test_product_page.py
@@ -26,6 +26,7 @@ class TestUserAddToBasketFromProductPage():
         product_page.open()
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
         product_page = ProductPage(browser, link2)
@@ -46,7 +47,8 @@ def test_guest_cant_see_success_message(browser):
     product_page.should_not_be_success_message()
 
 
-@pytest.mark.parametrize('promo', promo)
+@pytest.mark.parametrize('promo', ["0","1", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + promo
     product_page = ProductPage(browser, link)
@@ -87,6 +89,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -94,6 +97,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
